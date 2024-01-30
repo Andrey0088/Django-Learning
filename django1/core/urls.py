4ab856c2ddc0435bc079df9eb6e3c6c3158b543d
Andrey <66343094+Andrey0088@users.noreply.github.com>
2024-01-29 22:17:04 -0400

    Add files via upload

---
+++ django1/core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+#igual ao original
+
+from .views import index, contato
+#o ponto indica que é do próprio módulo core, do arquivo views, importa o arquivo/view index e contato.;
+
+#configurando rotas:
+urlpatterns = [
+    path('', index),
+    path('contato', contato),
+]
+#se for para a raiz, envia para index
+#se for para contato, envia para contato
+
+#assim tira as rotas do arquivo urls do projeto, e coloca aqui.
+#tudo que for para a raiz, manda para a aplicação core, ela que vai receber essas requisições.
+#toda requisição que for para a raiz, envia para a aplicação core.
